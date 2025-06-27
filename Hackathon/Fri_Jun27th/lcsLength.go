55b5764351d261f3b5a2bd9d3876794b2ea42671
Samuel Wang <samuelzixuan.wang1215@gmail.com>
2025-06-27 11:20:22 -0400

    pod 14

---
+++ Hackathon/Fri_Jun27th/lcsLength.go
@@ -0,0 +1,21 @@
+package main
+
+// LCSLength takes two strings as input. It returns the length of a longest common
+// subsequence of the two strings.
+func LCSLength(str1, str2 string) int {
+
+	return int
+}
+
+func Length() {
+
+}
+
+func Make2D_2[T any](n, m int) [][]T {
+	matrix := make([][]T, n)
+	for i := 0; i < n; i++ {
+		matrix[i] = make([]T, m)
+	}
+
+	return matrix
+}
