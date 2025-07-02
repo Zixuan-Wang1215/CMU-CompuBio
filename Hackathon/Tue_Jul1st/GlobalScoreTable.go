1f169498e6147c2a089589b9860633cc19497f4f
Samuel Wang <samuelzixuan.wang1215@gmail.com>
2025-07-02 09:43:09 -0400

    Co-authored-by: omamakos <omamakos@users.noreply.github.com> Co-authored-by: Walter Ralls <Smar-tees@users.noreply.github.com> Co-authored-by: michaelx0281 <michaelx0281@users.noreply.github.com>

--- Hackathon/Tue_Jul1st/GlobalScoreTable.go
+++ Hackathon/Tue_Jul1st/GlobalScoreTable.go
@@ -3,5 +3,28 @@ package main
 func GlobalScoreTable(str1, str2 string, match, mismatch, gap float64) [][]float64 {
 	matrix := Make2D_2[float64](len(str1)+1, len(str2)+1)
 
+
+	for i := range len(str1) {
+		if i == 0 {
+			//continue 
+		}
+		for j := 1; j < len(str2); j++ {
+			//CASE 1 --> matches
+			maxInt(matrix[i-1][j-1]+mew, matrix[])
+			//CASE 2 --> mismatches
+	}
+
 	return matrix
+}
+}
+
+func maxInt(ints...int) int {
+	max := 0
+	for i, val := range ints {
+		if i == 0 || val > max {
+			max = val
+		}
+	}
+
+	return val
 }
---
+++ Hackathon/Wed_Jul2nd/GlobalAlignment.go
@@ -0,0 +1,1 @@
+package wedjul2nd
