1520aaccb0cc3761eaf96701d7412e7890469cad
Samuel Wang <samuelzixuan.wang1215@gmail.com>
2025-07-09 10:47:41 -0400

    commit

--- Assembly/binarizeMatrix.go
+++ Assembly/binarizeMatrix.go
@@ -1,7 +1,7 @@
 package main
 
 // BinarizeMatrix takes a matrix of values and a threshold.
-// It binarizes the matrix according to the threshold.
+// It binarizes the matrix accordin g to the threshold.
 // If entries across main diagonal are both above threshold, only retain the bigger one.
 func BinarizeMatrix(mtx [][]float64, threshold float64) [][]int {
 
